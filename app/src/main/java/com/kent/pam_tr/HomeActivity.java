0b7390b73c58edb7fb541f79b68ed31ccdf562e5
GowellKent1@gmail.com <Gowellkent1@gmail.com>
2022-04-18 20:28:24 +0700

    fragment & navbar

--- app/src/main/java/com/kent/pam_tr/HomeActivity.java
+++ app/src/main/java/com/kent/pam_tr/HomeActivity.java
@@ -1,14 +1,56 @@
 package com.kent.pam_tr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch(item.getItemId()){
+                        case R.id.page_home:{
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.content_frame, new HomeFragment())
+                                    .commit();
+                        }
+                        break;
+                        case R.id.page_point:{
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.content_frame, new PointFragment())
+                                    .commit();
+                        }
+                        break;
+                        case R.id.page_profile: {
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.content_frame, new ProfileFragment())
+                                    .commit();
+                        }
+                        break;
+                    }
+                    return true;
+                }
+            };
 }
