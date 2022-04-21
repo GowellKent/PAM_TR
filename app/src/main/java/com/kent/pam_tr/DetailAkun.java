f403cdc95a34d8caefcc041772c2ead36a1c83dd
GowellKent1@gmail.com <Gowellkent1@gmail.com>
2022-04-21 19:14:57 +0700

    input poin

--- app/src/main/java/com/kent/pam_tr/DetailAkun.java
+++ app/src/main/java/com/kent/pam_tr/DetailAkun.java
@@ -6,22 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class DetailAkun extends AppCompatActivity {
 
     private TextView detNama, detNim, txtPoinOmb, txtPoinPro, txtPoinHum, txtPoinPen, txtPoinTotal;
     private FirebaseFirestore db;
+
+    private TextInputLayout txtInputPoin;
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button test;
+
+    int ombAwal, proAwal, humAwal, penAwal, totalAwal;
+    String docID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +50,8 @@ public class DetailAkun extends AppCompatActivity {
         txtPoinHum = findViewById(R.id.txtPoinHumDet);
         txtPoinPen = findViewById(R.id.txtPoinPenDet);
         txtPoinTotal = findViewById(R.id.txtPoinTotalDet);
+
+        txtInputPoin = findViewById(R.id.txtInputPoin);
 
         String namaa = getIntent().getStringExtra("nama");
 
@@ -53,6 +70,8 @@ public class DetailAkun extends AppCompatActivity {
                                 String nam = ds.get("nama").toString();
                                 String ni = ds.get("nim").toString();
 
+                                docID = ds.getId();
+
                                 detNama.setText("Nama : "+nam);
                                 detNim.setText("Nim : "+ni);
                                 txtPoinOmb.setText(poin.get("omb").toString());
@@ -60,11 +79,105 @@ public class DetailAkun extends AppCompatActivity {
                                 txtPoinHum.setText(poin.get("hum").toString());
                                 txtPoinPen.setText(poin.get("pen").toString());
                                 txtPoinTotal.setText(poin.get("total").toString());
+
+                                ombAwal = Integer.parseInt(poin.get("omb").toString());
+                                proAwal = Integer.parseInt(poin.get("pro").toString());
+                                humAwal = Integer.parseInt(poin.get("hum").toString());
+                                penAwal = Integer.parseInt(poin.get("hum").toString());
+                                totalAwal = Integer.parseInt(poin.get("total").toString());
                             }
                         }
 
                     }
                 });
 
+        ButtonListener();
+
+    }
+
+    public void ButtonListener(){
+        radioGroup = findViewById(R.id.radioGroup);
+        test = findViewById(R.id.tesbut);
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                radioButton = (RadioButton) findViewById(selectedId);
+
+                switch (selectedId){
+                    case R.id.radioOMB : {
+                        inputOMB();
+                    }break;
+                    case R.id.radioPro:  {
+                        inputPro();
+                    }break;
+                    case R.id.radioHum : {
+                        inputHum();
+                    }break;
+                    case R.id.radioPen : {
+                        inputPen();
+                    }break;
+                }
+
+            }
+        });
+    }
+
+    public void inputOMB(){
+
+        int poinTambah = Integer.parseInt(txtInputPoin.getEditText().getText().toString());
+
+        int ombBaru = poinTambah + ombAwal;
+        int totalBaru = ombBaru + proAwal + humAwal + penAwal;
+
+        db.collection("akun").document(docID).update("poin.omb", ombBaru);
+        db.collection("akun").document(docID).update("poin.total", totalBaru);
+
+        finish();
+        startActivity(getIntent());
+    }
+
+    public void inputPro(){
+
+        int poinTambah = Integer.parseInt(txtInputPoin.getEditText().getText().toString());
+
+        int proBaru = poinTambah + proAwal;
+        int totalBaru = ombAwal + proBaru + humAwal + penAwal;
+
+        db.collection("akun").document(docID).update("poin.pro", proBaru);
+        db.collection("akun").document(docID).update("poin.total", totalBaru);
+
+        finish();
+        startActivity(getIntent());
+    }
+
+    public void inputHum(){
+
+        int poinTambah = Integer.parseInt(txtInputPoin.getEditText().getText().toString());
+
+        int humBaru = poinTambah + humAwal;
+        int totalBaru = ombAwal + proAwal + humBaru + penAwal;
+
+        db.collection("akun").document(docID).update("poin.hum", humBaru);
+        db.collection("akun").document(docID).update("poin.total", totalBaru);
+
+        finish();
+        startActivity(getIntent());
+    }
+
+    public void inputPen(){
+
+        int poinTambah = Integer.parseInt(txtInputPoin.getEditText().getText().toString());
+
+        int penBaru = poinTambah + ombAwal;
+        int totalBaru = ombAwal + proAwal + humAwal + penBaru;
+
+        db.collection("akun").document(docID).update("poin.pen", penBaru);
+        db.collection("akun").document(docID).update("poin.total", totalBaru);
+
+        finish();
+        startActivity(getIntent());
     }
 }
